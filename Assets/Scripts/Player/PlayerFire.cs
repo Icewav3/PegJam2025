a4c0e541ca3e83cd8c6a9d2d1e890c20943b0665
Cate Runions <cate.runions@gmail.com>
2025-02-22 12:27:44 -0600

    make anima tor

--- Assets/Scripts/Player/PlayerFire.cs
+++ Assets/Scripts/Player/PlayerFire.cs
@@ -1,8 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
+    private bool _firing = false;
+
+    [SerializeField]
+    private Animator _anim;
     
+    public void StartFire()
+    {
+        if (_firing) return;
+
+        _firing = true;
+        _anim.SetTrigger("Fire");
+    }
+
+    public void StopFire()
+    {
+        _firing = false;
+    }
+
+    private void Update()
+    {
+        if(!_firing && _anim.GetCurrentAnimatorStateInfo(0).IsName("PlayerHoldFire"))
+        {
+            _anim.SetTrigger("ReleaseFire");
+        }
+    }
 }
