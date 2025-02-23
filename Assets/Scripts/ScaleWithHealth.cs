c164f24f2b90d9f0c153a7630537e56d260b2b73
Cate Runions <cate.runions@gmail.com>
2025-02-23 10:56:20 -0600

    add greeb enemy

--- Assets/Scripts/ScaleWithHealth.cs
+++ Assets/Scripts/ScaleWithHealth.cs
@@ -6,9 +6,11 @@ public class ScaleWithHealth : MonoBehaviour
 {
     [SerializeField]
     private HealthPool _hp;
+    [SerializeField]
+    private float _baseScale = 1f;
 
     private void Update()
     {
-        transform.localScale = Vector3.one * (1 + (0.1f * (_hp.Health - _hp.StartingHealth)));
+        transform.localScale = Vector3.one * (_baseScale + (0.1f * (_hp.Health - _hp.StartingHealth)));
     }
 }
