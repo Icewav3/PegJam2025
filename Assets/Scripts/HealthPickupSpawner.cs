b1fb2cd85730b26ac7cd682ad30c3ca14de40275
Cate Runions <cate.runions@gmail.com>
2025-02-23 12:54:41 -0600

    healt pickups

--- Assets/Scripts/HealthPickupSpawner.cs
+++ Assets/Scripts/HealthPickupSpawner.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class HealthPickupSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private HealthPickup _pickup;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private int _maxPickups = 5;
+
+    [SerializeField]
+    private float _arenaWidth = 25;
+
+    private List<HealthPickup> _pickups = new List<HealthPickup>();
+
+    private void Update()
     {
-        
+        if (_pickups.Count < _maxPickups)
+        {
+            Vector2 spawnPos = Random.insideUnitCircle * _arenaWidth;
+
+            if (Vector2.Distance(spawnPos, SceneGod.SInstance.player.transform.position) < 15) return;
+
+            HealthPickup hp = Instantiate(_pickup, spawnPos, Quaternion.identity);
+
+            _pickups.Add(hp);
+        }
+
+        _pickups = _pickups.Where(_pickup => _pickup != null).ToList();
     }
 }
